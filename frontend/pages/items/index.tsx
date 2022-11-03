83632ee01afcb6f65d5265b6ae3a62bfa82fdda9
Hidenori Matsuki <dev@mazgi.com>
2022-11-03 15:16:50 +0000

    create example pages.

---
+++ frontend/pages/items/index.tsx
@@ -0,0 +1,17 @@
+import { NextPage } from 'next'
+import Head from 'next/head'
+
+const Page: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Items</title>
+      </Head>
+      <main>
+        <h1>Items</h1>
+      </main>
+    </div>
+  )
+}
+
+export default Page
